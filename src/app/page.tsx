6b2d170daa7271a2e9b2c168d1bb661f5d435444
Lars Gregersen <lagr@videndjurs.dk>
2023-08-27 14:33:54 +0200

    pure_tailwind

---
+++ src/app/page.tsx
@@ -0,0 +1,48 @@
+"use client"
+
+import Image from 'next/image'
+import NinjaCard from './NinjaCard';
+import { actor } from "./type/types";
+import data from "./lib/actors.json";
+
+export default function Home() {
+
+  return (
+    <>
+
+      <div className="container mx-auto my-10">
+        <div
+          className="
+                   grid 
+                   grid-cols-1 
+                   md:grid-cols-2 
+                   lg:grid-cols-3
+                   gap-4 
+      
+                   mx-5 
+                   md:mx-2
+                   lg:mx-20
+                  "
+        >
+
+          {data &&
+            data.actors.map((item: actor, index: number) => (
+
+              <NinjaCard mdata={item} mindex={index} />
+
+            ))
+
+          }
+
+
+
+        </div>
+
+
+      </div>
+
+
+    </>
+
+  )
+}
---
+++ src/app/type/types.ts
@@ -0,0 +1,8 @@
+export interface actor {
+
+    id: string;
+    name: string;
+    image: string;
+    description: string;
+   
+  }
